43a7832805bec43f1a7c3b0bc18a917389bca492
luccabb <luccabertoncini@minerva.kgi.edu>
2021-12-15 04:05:09 -0800

    removing replicas from index

--- create_index.py
+++ create_index.py
@@ -5,8 +5,8 @@ API_URL = "https://rickandmortyapi.com/api/character"
 HOST = "localhost"
 PORT = 9200
 INDEX_NAME = "rickandmorty"
-PRIMARY_SHARDS = 3
-REPLICA_SHARDS = 2
+PRIMARY_SHARDS = 4
+REPLICA_SHARDS = 0
 
 # Create an Elasticsearch client connected
 # to localhost:9200
